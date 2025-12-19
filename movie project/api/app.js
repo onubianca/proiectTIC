1580643f655c84b41586ce714f6fc717271dd376
onubianca <147873021+onubianca@users.noreply.github.com>
2025-12-19 16:44:22 +0200

    Add files via upload

---
+++ movie project/api/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
+
+export default app;
